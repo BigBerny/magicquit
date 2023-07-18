8d7c350ca01908d76bae73145440b33e38ea0926
Janis Berneker <janis.berneker@typewise.app>
2023-07-19 01:23:14 +0200

    Minor improvements

--- MagicQuit/ContentView.swift
+++ MagicQuit/ContentView.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-//import AppKit
+import AppKit
 import Combine
 import UserNotifications
 
@@ -15,7 +15,7 @@ class RunningAppsManager: ObservableObject {
         }
     }
     @AppStorage("janisberneker.MagicQuit.toggleStatus") var toggleStatusData: Data = Data()
-
+    
     init() {
         syncToggleStatus()
         addCurrentRunningApps()
@@ -23,8 +23,8 @@ class RunningAppsManager: ObservableObject {
         print("init")
         let didDeactivateObserver = NSWorkspace.shared.notificationCenter
         didDeactivateObserver.addObserver(forName: NSWorkspace.didDeactivateApplicationNotification,
-                            object: nil, // always NSWorkspace
-                             queue: OperationQueue.main) { (notification: Notification) in
+                                          object: nil, // always NSWorkspace
+                                          queue: OperationQueue.main) { (notification: Notification) in
             print("didDeactivate")
             if let app = notification.userInfo?[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication {
                 DispatchQueue.main.async {
@@ -41,7 +41,7 @@ class RunningAppsManager: ObservableObject {
             }
         }
     }
-
+    
     deinit {
         print("RunningAppsManager is being deallocated")
     }
@@ -52,7 +52,7 @@ class RunningAppsManager: ObservableObject {
             toggleStatus = status
         }
     }
-
+    
     // Save toggleStatus to toggleStatusData
     func saveToggleStatus() {
         if let data = try? JSONEncoder().encode(toggleStatus) {
@@ -68,12 +68,12 @@ class RunningAppsManager: ObservableObject {
         }
         return true
     }
-
+    
     private func addCurrentRunningApps() {
         let workspace = NSWorkspace.shared
         let apps = workspace.runningApplications
         let currentDate = Date()
-
+        
         // Add new apps to runningApps
         for app in apps {
             if !isBlockedApp(app), self.runningApps[app] == nil {
@@ -83,18 +83,18 @@ class RunningAppsManager: ObservableObject {
             }
         }
     }
-
-
+    
+    
     private func checkOpenApps() {
         print("checkOpenApps")
         let workspace = NSWorkspace.shared
         let apps = workspace.runningApplications
         let currentDate = Date()
-
+        
         // Remove apps from runningApps that are not active anymore
         let currentApps = apps.compactMap { $0 }
         runningApps = runningApps.filter { currentApps.contains($0.key) }
-
+        
         // Set date of the currently active app to currentDate
         if let activeApp = workspace.frontmostApplication, !isBlockedApp(activeApp) {
             runningApps[activeApp] = currentDate
@@ -120,47 +120,52 @@ class RunningAppsManager: ObservableObject {
 
 struct ContentView: View {
     static let toggleStatusKey = "janisberneker.MagicQuit.toggleStatus"
-    @State private var isHovered = false
+    enum HoveredButton: Hashable {
+        case quit
+        case settings
+    }
+    @State private var hoveredButton: HoveredButton? = nil
+    @State private var showingSettings = false
+    @State private var settingsWindowController: SettingsWindowController?
     //@State private var toggleStatus: [String: Bool] = [:]
     //@AppStorage(ContentView.toggleStatusKey) private var toggleStatusData: Data = Data()
     @ObservedObject private var manager: RunningAppsManager
-
+    
     init(manager: RunningAppsManager) {
         self.manager = manager
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             VStack(spacing: 0) {
-                    ForEach(Array(manager.runningApps).sorted(by: { $0.0.localizedName! < $1.0.localizedName! }), id: \.0) { app in
-                        AppRow(app: app, manager: manager)
-                    }
+                ForEach(Array(manager.runningApps).sorted(by: { $0.0.localizedName! < $1.0.localizedName! }), id: \.0) { app in
+                    AppRow(app: app, manager: manager)
+                }
             }
             .padding(.horizontal, 10)
             .padding(.top, 5)
             .padding(.bottom, 0)
             Divider()
                 .padding(.horizontal, 10)
-                .padding(.vertical, 0)
+                .padding(.vertical, 5)
             Button(action: {
-                    NSApplication.shared.terminate(self)
-                }) {
-                    HStack {
-                        Text("Quit MagicQuit")
-                            .frame(maxWidth: .infinity, alignment: .leading) // aligns text to the leading edge
-                            .padding(.horizontal, 10) // adds padding to the leading edge of the text
-                            .padding(.vertical, 5)
-                            .foregroundColor(isHovered ? Color.white : Color.primary) // Change text color to white when hovering
-                        
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity)
-                    //.padding(.vertical, 10) // adds vertical padding to the entire button
+                settingsWindowController = SettingsWindowController(rootView: SettingsView())
+                NSApp.activate(ignoringOtherApps: true) // Activate the application
+                settingsWindowController?.showWindow(nil)
+            }) {
+                HStack {
+                    Text("Settings")
+                        .frame(maxWidth: .infinity, alignment: .leading) // aligns text to the leading edge
+                        .padding(.horizontal, 10) // adds padding to the leading edge of the text
+                        .padding(.vertical, 5)
+                        .foregroundColor(hoveredButton == .settings ? Color.white : Color.primary) // Change text color to white when hovering
+                    
                 }
-                .buttonStyle(PlainButtonStyle())
+                .frame(maxWidth: .infinity)
+                //.padding(.vertical, 10) // adds vertical padding to the entire button
                 .background(
                     Group {
-                        if isHovered {
+                        if hoveredButton == .settings {
                             RoundedRectangle(cornerRadius: 5)
                                 .fill(Color.blue)
                         } else {
@@ -170,12 +175,47 @@ struct ContentView: View {
                     }
                 )
                 .onHover { hovering in
-                    isHovered = hovering
+                    hoveredButton = hovering ? .settings : nil
                 }
-
             }
+            .contentShape(Rectangle())
+            .buttonStyle(PlainButtonStyle())
+            Button(action: {
+                NSApplication.shared.terminate(self)
+            }) {
+                HStack {
+                    Text("Quit MagicQuit")
+                        .frame(maxWidth: .infinity, alignment: .leading) // aligns text to the leading edge
+                        .padding(.horizontal, 10) // adds padding to the leading edge of the text
+                        .padding(.vertical, 5)
+                        .foregroundColor(hoveredButton == .quit ? Color.white : Color.primary) // Change text color to white when hovering
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                //.padding(.vertical, 10) // adds vertical padding to the entire button
+                .background(
+                    Group {
+                        if hoveredButton == .quit {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.blue)
+                        } else {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.clear)
+                        }
+                    }
+                )
+                .onHover { hovering in
+                    hoveredButton = hovering ? .quit : nil
+                }
+            }.contentShape(Rectangle())
+                .buttonStyle(PlainButtonStyle())
+            
+        }
+        
         .frame(width: 300)
         .padding(5)
+        
     }
 }
 
@@ -192,7 +232,7 @@ struct AppRow: View {
             }
         )
     }
-
+    
     var body: some View {
         let secondsUntilClose = (RunningAppsManager.hoursUntilClose * 60 * 60) - Int(Date().timeIntervalSince(app.value))
         let isLessThanHour = secondsUntilClose < 3600
@@ -204,12 +244,12 @@ struct AppRow: View {
             .frame(alignment: .leading)
             
             let icon = NSWorkspace.shared.icon(forFile: app.key.bundleURL?.path ?? "")
-
+            
             Image(nsImage: icon)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 28, height: 28)
-
+            
             Text(app.key.localizedName ?? "Unknown")
                 .frame(minWidth: 100, maxWidth: .infinity, alignment: .leading)
                 .fontWeight(isLessThanHour ? .bold : .regular)
@@ -226,21 +266,30 @@ struct AppRow: View {
                         dimension[.trailing]
                     })
             }
-
+            
             Button(action: {
-                // Define your star button action here
-                print("\(app.key.localizedName ?? "") star button pressed")
+                // Set date of the app to now
                 manager.runningApps[app.0] = Date()
             }) {
                 Image(systemName: "arrow.uturn.backward.circle") // Use SF Symbols for the star icon
             }
             .buttonStyle(PlainButtonStyle())
             .frame(alignment: .trailing)
-            .foregroundColor(isOn.wrappedValue ? .primary : .gray) // Change the text color based on the toggle status
+            //.foregroundColor(isOn.wrappedValue ? .primary : .gray) // Change the text color based on the toggle status
+            .disabled(!isOn.wrappedValue) // Disable the button if the checkbox is not checked
+            /*Button(action: {
+             // Close the app
+             app.key.terminate()
+             }) {
+             Image(systemName: "x.circle") // Use SF Symbols for the star icon
+             }
+             .buttonStyle(PlainButtonStyle())
+             .frame(alignment: .trailing)
+             .foregroundColor(isOn.wrappedValue ? .primary : .gray) // Change the text color based on the toggle status*/
         }
         .frame(maxWidth: .infinity)
     }
-
+    
     // your formatTime function here
     private func formatTime(seconds: Int) -> String {
         if seconds >= 3600 {
@@ -252,6 +301,39 @@ struct AppRow: View {
         }
     }
 }
+
+class SettingsWindowController: NSWindowController {
+    convenience init(rootView: SettingsView) {
+        let hostingController = NSHostingController(rootView: rootView.frame(width: 400, height: 200))
+        let window = NSWindow(contentViewController: hostingController)
+        window.title = "Settings"
+        self.init(window: window)
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+                .padding()
+            
+            Text("Settings content goes here")
+                .padding()
+        }
+    }
+}
+
+struct SettingsWindow: NSViewRepresentable {
+    func makeNSView(context: Context) -> NSView {
+        let settingsWindowController = SettingsWindowController(rootView: SettingsView())
+        settingsWindowController.showWindow(nil)
+        return NSView()
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {}
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- MagicQuit/MagicQuitApp.swift
+++ MagicQuit/MagicQuitApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AppKit
 
 let runningAppsManager = RunningAppsManager()
 
